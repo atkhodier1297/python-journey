11fcdde370b3428726115a7ca377e55d87d1f662
Adam Khodier <khodieradam1297@gmail.com>
2024-12-30 18:57:32 -0500

    intro to python

---
+++ app.py/main.py
@@ -0,0 +1,29 @@
+# Printing in Python.
+
+print('Hello World, I am Adam Khodier')
+print('o----')
+print(' ||||')
+
+print('*' * 10)
+# Python interpreter evaluating our expression on line 4 creates 10 asterisks.
+
+# Python variables. Integers/Floats/Boolean just like JS
+
+price = 10
+price = 20
+rating = 4.9
+is_published = True
+print(price)
+print(is_published)
+
+name = input('What is your name? ')
+fav_color = input('What is your favorite color? ')
+print(name + ' likes ' + fav_color)
+
+birth_year = input('Birth year: ')
+print(type(birth_year))
+age = 2024 - int(birth_year)
+print(type(age))
+print(age)
+# Input function in Py is always a string so if you expect an integer value make sure to convert it to a integer or float.
+
